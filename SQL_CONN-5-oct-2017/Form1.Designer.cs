9125fb2c4656550a8e30cfff0c245eee7a16ec60
Duncan Sterken <duncan.sterken123@gmail.com>
2019-01-11 11:43:23 +0100

    Init

---
+++ SQL_CONN-5-oct-2017/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SQL_CONN_5_oct_2017 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txbCurrTextDSte = new System.Windows.Forms.TextBox();
+            this.btnSaveTextDSte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbCurrTextDSte
+            // 
+            this.txbCurrTextDSte.Location = new System.Drawing.Point(12, 12);
+            this.txbCurrTextDSte.Name = "txbCurrTextDSte";
+            this.txbCurrTextDSte.Size = new System.Drawing.Size(260, 20);
+            this.txbCurrTextDSte.TabIndex = 1;
+            // 
+            // btnSaveTextDSte
+            // 
+            this.btnSaveTextDSte.Location = new System.Drawing.Point(12, 39);
+            this.btnSaveTextDSte.Name = "btnSaveTextDSte";
+            this.btnSaveTextDSte.Size = new System.Drawing.Size(260, 30);
+            this.btnSaveTextDSte.TabIndex = 2;
+            this.btnSaveTextDSte.Text = "Save";
+            this.btnSaveTextDSte.UseVisualStyleBackColor = true;
+            this.btnSaveTextDSte.Click += new System.EventHandler(this.btnSaveTextDSte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 81);
+            this.Controls.Add(this.btnSaveTextDSte);
+            this.Controls.Add(this.txbCurrTextDSte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbCurrTextDSte;
+        private System.Windows.Forms.Button btnSaveTextDSte;
+    }
+}
+
---
+++ SQL_CONN-5-oct-2017/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Add MySql Library
+using MySql.Data.MySqlClient;
+
+namespace SQL_CONN_5_oct_2017 {
+    public partial class Form1 : Form {
+
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private string rowId;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        //Initialize values
+        private void Initialize() {
+            server = "localhost";
+            database = "sharp";
+            uid = "root";
+            password = "";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection() {
+            try {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex) {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number) {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection() {
+            try {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex) {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Select statement
+        public List<string>[] GetText() {
+            string query = "SELECT * FROM test";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[2];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true) {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read()) {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["text"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            } else {
+                return list;
+            }
+        }
+
+        //Update statement
+        public void UpdateData(String row, String newText) {
+            string query = "UPDATE test SET text=@inputText WHERE id=@id";
+
+            //Open connection
+            if (this.OpenConnection() == true) {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Set the variables here for more security
+                cmd.Parameters.AddWithValue("@inputText", newText);
+                cmd.Parameters.AddWithValue("@id", row);
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+                
+                //Prepare the statement
+                cmd.Prepare();
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+                MessageBox.Show("Saved", "Information");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Initialize();
+            txbCurrTextDSte.Text = this.GetText()[1][0];
+            this.rowId = this.GetText()[0][0];
+        }
+
+        private void btnSaveTextDSte_Click(object sender, EventArgs e) {
+            //Save text
+            this.UpdateData(this.rowId, txbCurrTextDSte.Text);
+        }
+    }
+}
